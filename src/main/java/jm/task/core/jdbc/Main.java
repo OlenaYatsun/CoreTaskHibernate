6fcc2a5b02ed0cc7ef02e93692954c6ac21fc356
OlenaYatsun <86918557+OlenaYatsun@users.noreply.github.com>
2022-01-20 17:03:34 +0900

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,16 @@
 package jm.task.core.jdbc;
-
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class Main {
 
     public static void main(String[] args)  {
-        UserDaoJDBCImpl user = new UserDaoJDBCImpl();
+        UserServiceImpl user = new UserServiceImpl();
         user.createUsersTable();
-        user.saveUser("Ivan", "Pupkin", (byte) 29);
-        user.saveUser("Irina", "Pupkina", (byte) 31);
-        user.saveUser("Hoon", "Byeon", (byte) 31);
-        user.saveUser("Vasilisa", "Socolova", (byte) 28);
-        user.getAllUsers();
-        user.removeUserById(1);
-        user.cleanUsersTable();
-        user.dropUsersTable();
+        user.saveUser("Ivan","Pupkin", (byte) 30);
 
-
-
+        }
     }
-}
